385b9ff16a09613f749d9fa5562d10ba9df39659
vit ruller <vit@therodina.com>
2017-11-28 14:16:36 +0100

    fixed basic js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 console.log('JS running');
+import lazysizes from 'lazysizes';
+import objectFitImages from 'object-fit-images';
+import Flickity from 'flickity';
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  cellAlign: 'center',
+  contain: true,
+  pageDots: false,
+  wrapAround: true,
+  setGallerySize: false
+});
